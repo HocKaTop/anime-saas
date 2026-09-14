6974f99c20a539f3aa4b25a5ac14a62927d0cb74
HocKaTop <hockatop@icloud.com>
2026-09-14 12:08:04 +0300

    В browse_screen имплементирован title_card, данные берутся из mock_data, горизонтальный скролл списка доступных тайтлов

--- lib/screens/browse_screen.dart
+++ lib/screens/browse_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../data/mock_data.dart';
+import '../widgets/title_card.dart';
 
 class BrowseScreen extends StatelessWidget {
   const new({super.key});
@@ -9,9 +11,65 @@ class BrowseScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Creepy Oleg"),
       ),
-      body: const Center(
-        child: Text("Главная"),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical:16),
+        children:[
+          _buildSection(
+            context,
+            title: 'Популярное сейчас',
+            titles: mockTitles,
+          ),
+
+          const SizedBox( height:24),
+          _buildSection(
+            context,
+            title: 'Новые релизы',
+            titles: mockTitles.reversed.toList(),
+          ),
+
+          const SizedBox(height:24),
+          _buildSection(
+            context,
+            title: 'Рекомендуем',
+            titles: mockTitles,
+          ),
+        ],
       ),
     );
   }
+  Widget _buildSection(
+    BuildContext context, {
+      required String title,
+      required List<AnimeTitle> titles,
+    }) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal:18),
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.titleLarge,
+              ),
+          ),
+          const SizedBox(height: 12),
+          SizedBox(
+            height: 300,
+            child:ListView.separated(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.symmetric(horizontal : 16),
+              itemCount: titles.length,
+              separatorBuilder:(context, index ){
+                return const SizedBox(width: 8);
+              },
+              itemBuilder: (context, index){
+                return TitleCard(
+                  anime: titles[index],
+                  );
+              },
+            )
+          )
+        ],
+      );
+    }
 }
